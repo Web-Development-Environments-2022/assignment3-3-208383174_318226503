2f15d1010c7de2a1c8ede0484260a6cb23c4a105
ShahafErez <Shahaf30@gmail.com>
2022-06-21 16:28:18 +0300

    moved to component

---
+++ src/components/FavoriteButton.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <b-icon
+      v-if="recipe.isFavorite === false"
+      class="heart_icon"
+      icon="heart"
+      font-scale="2"
+    ></b-icon>
+
+    <b-icon
+      v-if="recipe.isFavorite === true"
+      class="heart_icon"
+      icon="heart-fill"
+      variant="danger"
+      font-scale="2"
+    ></b-icon>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FavoriteButton",
+  props: {
+    recipe: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {};
+  },
+  mounted() {},
+  methods: {},
+};
+</script>
+
+<style scoped></style>
