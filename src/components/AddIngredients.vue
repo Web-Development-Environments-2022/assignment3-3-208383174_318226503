da8d3f5ed6f2ce4726889ec32b5a5c78ab859643
ShahafErez <Shahaf30@gmail.com>
2022-06-24 03:23:54 +0300

    wip- adding ingredients

---
+++ src/components/AddIngredients.vue
@@ -0,0 +1,89 @@
+<template>
+  <div>
+  <b-form inline @submit.prevent="onSubmit">
+    <label class="sr-only" for="inline-form-input-name">Name</label>
+    <b-form-input
+      id="input-name"
+      v-model="form.name"
+      class="mb-2 mr-sm-2 mb-sm-0"
+      placeholder="Name"
+    ></b-form-input>
+
+    <label class="sr-only" for="inline-form-input-name">Amount</label>
+    <b-form-input
+      id="input-amount"
+      v-model="form.amount"
+      class="mb-2 mr-sm-2 mb-sm-0"
+      placeholder="Amount"
+
+    ></b-form-input>
+    <label class="sr-only" for="inline-form-input-name">Unit</label>
+    <b-form-input
+      id="input-unit"
+      v-model="form.unit"
+      class="mb-2 mr-sm-2 mb-sm-0"
+      placeholder="Unit"
+
+    ></b-form-input>
+
+      <b-button
+        type="submit"
+        variant="primary"
+        >Add</b-button
+      >
+  </b-form>
+</div>
+</template>
+
+<script>
+export default {
+  name: "AddIngredients",
+   props: {
+    name: Text,
+    amount: Number,
+    unit: Number,
+  },
+  created(){
+    this.ingridents=[];
+  },
+ data() {
+      return {
+        form: {
+         name: "",
+        amount: null,
+        unit: "",
+        ingridents: [],
+        mounted: false
+        },
+      }
+    },
+    mounted() {
+  this.mounted = true;
+},
+    methods: {
+      onSubmit(event) {
+        console.log("you sumbitted");
+        event.preventDefault()
+        alert(JSON.stringify(this.form))
+        console.log("emitting")
+        console.log(this.ingridents);
+        this.ingridents.push(9);
+        this.ingridents.push({
+          name:this.form.name, amount:this.form.amount, unit:this.form.unit,
+        })
+        console.log(this.ingridents);
+        this.$emit(
+          this.form.name, this.form.amount, this.form.unit,
+        )
+               this.form={
+        name: "",
+        amount: null,
+        unit: "",
+       }
+      },
+
+    }
+};
+</script>
+
+<style scoped>
--- src/components/NewRecipe.vue
+++ src/components/NewRecipe.vue
@@ -1,106 +1,139 @@
 <template>
   <div>
     <b-modal id="modal-1" title="Create Your New Recipe">
-     <div>
-    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+      <div>
+        <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+          <!-- title -->
+          <b-form-group id="title" label="Title:" label-for="input-1">
+            <b-form-input
+              id="input-1"
+              v-model="form.title"
+              type="text"
+              placeholder="Enter your recipe title"
+              required
+            ></b-form-input>
+          </b-form-group>
 
-      <!-- title -->
-      <b-form-group
-        id="title"
-        label="Title:"
-        label-for="input-1"
-      >
-        <b-form-input
-          id="input-1"
-          v-model="form.title"
-          type="text"
-          placeholder="Enter your recipe title"
-          required
-        ></b-form-input>
-      </b-form-group>
+          <!-- image -->
+          <b-form-group
+            id="input-group-2"
+            label="Image url:"
+            label-for="input-2"
+          >
+            <b-form-input
+              id="input-2"
+              v-model="form.image"
+              placeholder="Enter your recipe's image url"
+              required
+            ></b-form-input>
+          </b-form-group>
 
-      <!-- image -->
-      <b-form-group id="input-group-2" label="Image url:" label-for="input-2">
-        <b-form-input
-          id="input-2"
-          v-model="form.image"
-          placeholder="Enter your recipe's image url"
-          required
-        ></b-form-input>
-      </b-form-group>
+          <!-- readyInMinutes -->
+          <b-form-group
+            id="input-group-3"
+            label="Making time:"
+            label-for="input-3"
+          >
+            <b-form-input
+              id="input-3"
+              type="number"
+              v-model="form.readyInMinutes"
+              placeholder="How many minutes does it takes to make the recipe"
+              required
+            ></b-form-input>
+          </b-form-group>
 
-      <!-- readyInMinutes -->
-      <b-form-group id="input-group-3" label="Making time:" label-for="input-3">
-        <b-form-input
-          id="input-3"
-          type="number"
-          v-model="form.readyInMinutes"
-          placeholder="How many minutes does it takes to make the recipe"
-          required
-        ></b-form-input>
-      </b-form-group>
+          <b-form-group
+            label="select if the following maches your recipe"
+            id="input-group-4"
+            v-slot="{ ariaDescribedby }"
+          >
+            <b-form-checkbox-group
+              v-model="form.checked"
+              id="checkboxes-4"
+              :aria-describedby="ariaDescribedby"
+            >
+              <b-form-checkbox value="vegan">Vegan</b-form-checkbox>
+              <b-form-checkbox value="vegeterian">Vegeterian</b-form-checkbox>
+              <b-form-checkbox value="gluten-free">Gluten Free</b-form-checkbox>
+            </b-form-checkbox-group>
+            <h2>ingredients</h2>
+            <AddIngredients
+              ref="ing"
+              v-model="form.extendedIngredients"
+              :key="1"
+            />
+            <template v-if="mounted">{{ $refs.ing.value }}</template>
+          </b-form-group>
 
-      <b-form-group label="select if the following maches your recipe" id="input-group-4" v-slot="{ ariaDescribedby } ">
-        <b-form-checkbox-group
-          v-model="form.checked"
-          id="checkboxes-4"
-          :aria-describedby="ariaDescribedby"
-        >
-          <b-form-checkbox value="vegan">Vegan</b-form-checkbox>
-          <b-form-checkbox value="vegeterian">Vegeterian</b-form-checkbox>
-          <b-form-checkbox value="gluten-free">Gluten Free</b-form-checkbox>
-        </b-form-checkbox-group>
-      </b-form-group>
-
-      <b-button type="submit" variant="primary">Submit</b-button>
-      <b-button type="reset" variant="danger">Reset</b-button>
-    </b-form>
-    <b-card class="mt-3" header="Form Data Result">
-      <pre class="m-0">{{ form }}</pre>
-    </b-card>
-  </div>
+          <b-button type="submit" variant="primary">Submit</b-button>
+          <b-button type="reset" variant="danger">Reset</b-button>
+        </b-form>
+      </div>
+      <template #modal-footer="{ cancel }">
+        <b-button size="sm" @click="cancel()">
+          close
+        </b-button>
+      </template>
     </b-modal>
   </div>
 </template>
 
 <script>
+import AddIngredients from "./AddIngredients.vue";
 export default {
+  components: { AddIngredients },
   name: "NewRecipe",
- data() {
-      return {
-        form: {
-          title: '',
-          image: '',
-          readyInMinutes: null,
-          vegan: null,
-          vegetarian: null,
-          glutenFree: null,
-          checked: []
-        },
-        foods: [{ text: 'Select One', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn'],
-        show: true
-      }
-    },
-    methods: {
-      onSubmit(event) {
-        event.preventDefault()
-        alert(JSON.stringify(this.form))
+  props: {
+    name: Text,
+    amount: Number,
+    unit: Number,
+  },
+  data() {
+    return {
+      form: {
+        title: "",
+        image: "",
+        servingSize: "",
+        first_time: "",
+        readyInMinutes: null,
+        vegan: null,
+        vegetarian: null,
+        glutenFree: null,
+        extendedIngredients: [],
+        analyzedInstructions: null,
+        checked: [],
       },
-      onReset(event) {
-        event.preventDefault()
-        // Reset our form values
-        this.form.email = ''
-        this.form.name = ''
-        this.form.food = null
-        this.form.checked = []
-        // Trick to reset/clear native browser form validation state
-        this.show = false
-        this.$nextTick(() => {
-          this.show = true
-        })
-      }
-    }
+
+      show: true,
+    };
+  },
+  methods: {
+    onSubmit(event) {
+      event.preventDefault();
+      alert(JSON.stringify(this.form));
+    },
+    onReset(event) {
+      event.preventDefault();
+      // Reset our form values
+      this.form.email = "";
+      this.form.name = "";
+      this.form.food = null;
+      this.form.checked = [];
+      // Trick to reset/clear native browser form validation state
+      this.show = false;
+      this.$nextTick(() => {
+        this.show = true;
+      });
+    },
+  },
 };
 </script>
 
-<style scoped>
+<style scope></style>
+
+<style>
+#modal-1 .modal-dialog {
+  max-width: 800px;
+}
+</style>
