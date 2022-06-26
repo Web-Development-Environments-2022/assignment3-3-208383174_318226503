b63679d12f18883b5ed3c4b3b352ae66645d0d3a
ShahafErez <Shahaf30@gmail.com>
2022-06-26 20:59:55 +0300

    404

--- src/pages/NotFoundPage.vue
+++ src/pages/NotFoundPage.vue
@@ -1,6 +1,29 @@
 <template>
   <div class="container">
-    <h1>Four Oh Four you didn't</h1>
-    <router-link to="/" exact>ET Go Home</router-link>
+    <h1>sorry, we haven't developed this peacher yet</h1>
+    <div>
+      <img src="../../resources/sad.png" style="width: 150px" />
+    </div>
+    <router-link class="home" to="/" exact>ET Go Home</router-link>
   </div>
 </template>
+
+<style scoped>
+h1 {
+  margin-top: 25px;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+}
+
+.home {
+  font-size: 23px;
+  color: rgb(238, 138, 30);
+}
+
+img {
+  margin-bottom: 10px;
+}
+
+.container {
+  text-align: center;
+}
+</style>
